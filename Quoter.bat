235a02a697f6e0aed8c8fc45a2802d8f5c2a5b1a
m8tan <alt6526@gmail.com>
2025-10-20 09:41:24 +0300

    Added some style to the app

--- Quoter.bat
+++ Quoter.bat
@@ -1,5 +1,13 @@
 @echo off
+setlocal enabledelayedexpansion
 title Quoter
+for /l %%i in (1,1,5) do (
+    set "line="
+    for /L %%i in (1,1,10) do (
+        set "line=!line!#"
+        echo !line!
+    )
+)
 cd .\
 cls
 ruby Quoter.rb
